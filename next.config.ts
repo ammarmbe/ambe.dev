9f192ac39a5611e671f969087371e9e2b816f826
Ammar Elbehery <ammarelbehery04@gmail.com>
2025-10-29 18:16:26 +0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/vibe",
+        destination: "/projects/vibe",
+      },
+      {
+        source: "/tagged",
+        destination: "/projects/tagged",
+      },
+      {
+        source: "/curaflow",
+        destination: "/projects/curaflow",
+      },
+    ]
+  },
 };
 
 export default nextConfig;
